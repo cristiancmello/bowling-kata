eaa42a4a71d1cec273b5d8619e127abfdf99dcab
Cristian Mello <cristianc.mello@gmail.com>
2023-10-13 14:01:51 -0300

    First commit

--- src/main/java/org/pandasdojo/Main.java
+++ src/main/java/org/pandasdojo/Main.java
@@ -1,4 +1,4 @@
-package org.quickstart;
+package org.pandasdojo;
 
 public class Main {
   public static void main(String[] args) {
--- src/test/java/org/quickstart/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package org.quickstart;
-
-import org.junit.jupiter.api.Test;
-
-public class ExampleTest {
-  @Test
-  public void nothing() {
-  }
-}
